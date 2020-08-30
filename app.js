ac0095e4464bfcff217adf5793b70f65e7a504e8
MohinishTeja <45705872+MohinishTeja@users.noreply.github.com>
2020-08-30 15:30:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x8886,
+  shininess: 90.00,
+  waveHeight: 18.50,
+  waveSpeed: 1.25,
+  zoom: 0.65
+})
+
